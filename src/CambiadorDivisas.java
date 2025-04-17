6f134d06a1f7ebf1e77b8988b07184d0e26e9ea0
Tomas Sedares <tomassedares@gmail.com>
2025-04-16 21:28:26 -0300

    agregué comentarios

--- src/CambiadorDivisas.java
+++ src/CambiadorDivisas.java
@@ -13,21 +13,28 @@ public class CambiadorDivisas {
     private double valorDivisa;
 
     public void cambiarDivisas(String monedaBase, String monedaDestino) throws IOException, InterruptedException {
+        //concateno la divisa que quiero buscar con la parte de la URL de la API
         String url = "https://v6.exchangerate-api.com/v6/57742dc9edc66acb688b0561/latest/" + monedaBase;
 
         try {
+            //creo el cliente y la request
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(url))
                     .build();
 
+            //creo el response
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+            //creo el gson, el cual sera mi intermediario entre el json y un objeto de java
             Gson gson = new Gson();
 
+            //convierto el json en un objeto java
             JsonObject jsonObject = gson.fromJson(response.body(), JsonObject.class);
+            //creo un objeto el cual va a contener SOLAMENTE las partes del converio_rates
             JsonObject conversiones = jsonObject.getAsJsonObject("conversion_rates");
 
+            //guardo en una variable la moneda a la cual queria convertir mis divisas (monedaDestino) extrayendolas directamente del objeto json
             valorDivisa = conversiones.get(monedaDestino).getAsDouble();
 
         } catch (IOException | InterruptedException e) {
@@ -35,6 +42,7 @@ public class CambiadorDivisas {
         }
     }
 
+    //calculo el valor final al transformar de una divisa a otra
     public void calcularDivisas(int cantidad) {
         total = cantidad * valorDivisa;
     }
--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,11 @@ public class Main {
         int respuesta;
         int monto;
 
+        //instancio mis 2 clases para poder ustilizar sus respectivos metodos
         Menues menues = new Menues();
         CambiadorDivisas currencyExchanger = new CambiadorDivisas();
 
+        //creo un ciclo while que ejecute continuamente los menues mientras el usuario elija una opcion valida
         while (true) {
             System.out.println(menues.menuInicial());
             respuesta = scan.nextInt();
@@ -23,12 +25,20 @@ public class Main {
                 break;
             }
 
+            //el switch analiza que opcion se elijio y actua en consecuencia
             switch (respuesta) {
                 case 1:
+                    //muestro el menu
                     System.out.println(menues.textoIngresarDivisas());
+                    //permito al usuario elegir la disvisa
                     monto = scan.nextInt();
+                    //le paso por parametro las monedas que posteriormente se le consultara a la id
+                    //la moneda base pide una requset en la que agarra todos los tipos de cambio que tiene en este caso el dolar
+                    //posteriormente me da un objeto para que despues pueda buscar con monedaDestino la moneda la cual quiero calcular
                     currencyExchanger.cambiarDivisas("USD", "ARS");
+                    //realizo el calculo multiplicando la cantidad de divisas ingresadas con el valor de la divisa que me dio el json
                     currencyExchanger.calcularDivisas(monto);
+                    //muestro el resultado final
                     System.out.println(currencyExchanger);
                     break;
                 case 2:
